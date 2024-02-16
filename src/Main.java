49a12d4be3e5fc2f5cf74ab46815fefd4f566150
Prog- <elizardo.powell@uip.edu.pa>
2024-02-16 18:59:28 -0500

    Informacion de visistante

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,34 @@ public class Main{
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("El visitante es un empleado? Si/No");
-        boolean esEmpleado = scanner.nextBoolean();
+            boolean esEmpleado = scanner.nextBoolean();
+
         System.out.println("Es un visitante Nuevo (Si/No)");
-        boolean esVisitanteNuevo = scanner.nextBoolean();
+             boolean esVisitanteNuevo = scanner.nextBoolean();
+
         System.out.println("nombre del visitante");
-        String nombre = scanner.next();
+            String nombre = scanner.next();
+
         System.out.println("Edad del visitante");
-        byte Edad = scanner.nextByte();
+             byte Edad = scanner.nextByte();
+
         System.out.println("Genero del visitante");
-        char Genero = scanner.next().charAt(0);
+            char Genero = scanner.next().charAt(0);
+
         System.out.println("Introduzca peso del visitante (En Kg)");
-        Short Peso = scanner.nextShort();
+            Short Peso = scanner.nextShort();
+
         System.out.println("Ingresse altura del visitante (En cm)");
-        int altura = scanner.nextInt();
+            int altura = scanner.nextInt();
+
         System.out.println("Ingrese Numero de telefono");
-        long telefono = scanner.nextLong();
+            long telefono = scanner.nextLong();
+
         System.out.println("Numero de Apartamento");
-        int Apartamento = scanner.nextInt();
+             int Apartamento = scanner.nextInt();
+
         System.out.println("Numero de visitantes");
-        int Visitantes = scanner.nextInt();
+            int Visitantes = scanner.nextInt();
 
 
         //Se muestra en pantalla
