6c037f99ac4b2da032d0ce78df30654d51a106e5
Prog- <elizardo.powell@uip.edu.pa>
2024-02-16 19:03:39 -0500

    Informacion de visistante

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,50 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("El visitante es un empleado? Si/No");
-            boolean esEmpleado = scanner.nextBoolean();
+        boolean esEmpleado = scanner.nextBoolean();
 
         System.out.println("Es un visitante Nuevo (Si/No)");
-             boolean esVisitanteNuevo = scanner.nextBoolean();
+        boolean esVisitanteNuevo = scanner.nextBoolean();
 
         System.out.println("nombre del visitante");
-            String nombre = scanner.next();
+        String nombre = scanner.next();
 
         System.out.println("Edad del visitante");
-             byte Edad = scanner.nextByte();
+        byte Edad = scanner.nextByte();
 
         System.out.println("Genero del visitante");
-            char Genero = scanner.next().charAt(0);
+        char Genero = scanner.next().charAt(0);
 
         System.out.println("Introduzca peso del visitante (En Kg)");
-            Short Peso = scanner.nextShort();
+        Short Peso = scanner.nextShort();
 
         System.out.println("Ingresse altura del visitante (En cm)");
-            int altura = scanner.nextInt();
+        int altura = scanner.nextInt();
 
         System.out.println("Ingrese Numero de telefono");
-            long telefono = scanner.nextLong();
+        long telefono = scanner.nextLong();
 
         System.out.println("Numero de Apartamento");
-             int Apartamento = scanner.nextInt();
+        int Apartamento = scanner.nextInt();
 
         System.out.println("Numero de visitantes");
-            int Visitantes = scanner.nextInt();
-
+        int Visitantes = scanner.nextInt();
 
         //Se muestra en pantalla
         System.out.println("Informacion Del Visitante");
         System.out.println("Nombre: " + nombre);
-        System.out.println("Numero De Apartamento: " + Apartamento);
         System.out.println("Edad: " + Edad);
+        System.out.println("Telefono: " + telefono);
+        System.out.println("Peso del visitante: " + Peso);
+        System.out.println("Altura del visitante: " + altura);
         System.out.println("Genero: " + Genero);
         System.out.println("Es su Primera Visita: " + esVisitanteNuevo);
         System.out.println("Cantidad de Visitantes: " + Visitantes);
+        System.out.println("Numero De Apartamento: " + Apartamento);
     }
 }
